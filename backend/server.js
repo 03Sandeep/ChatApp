59ecf807c20e9b5875436c42c8d143a427ed889b
Sandeep Sinha <sandeepsinha1210@gmail.com>
2024-06-24 08:23:40 +0530

    chat model updation

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 const express = require("express");
-const chats = require("./data/data.js");
-const env = require("dotenv");
+// const chats = require("./data/data.js");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+const userRoutes = require("./routes/userRoutes.js");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
-env.config();
+dotenv.config();
+connectDB();
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Yoyo");
-});
-const port = process.env.PORT;
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log("Server started on Port 5000");
+  console.log(`Server started on Port ${port}`);
 });
